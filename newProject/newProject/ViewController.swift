c91657956fbe9cbaf5f9572e6d283adfbc4039cc
shadon2024 <in-con23@yandex.ru>
2024-04-25 19:37:39 +0500

    new

---
+++ newProject/newProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  newProject
+//
+//  Created by Admin on 25/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
